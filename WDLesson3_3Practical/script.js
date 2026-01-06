5e8639c0fa725cd537071f42dcfd128883f64e92
noob6666666 <oweny19@nycstudents.net>
2026-01-06 19:01:59 +0000

    new

--- WDLesson3_3Practical/script.js
+++ WDLesson3_3Practical/script.js
@@ -1,1 +1,25 @@
 /* Challenge 2: Create a function for each image that display a description of the picture into the div with an id of highlight*/
+function showCat1() {
+  document.getElementById("highlight").innerHTML =
+    "A close-up of a cat.";
+}
+
+function showCat2() {
+  document.getElementById("highlight").innerHTML =
+    "A relaxed house cat resting comfortably.";
+}
+
+function showCat3() {
+  document.getElementById("highlight").innerHTML =
+    "A weird cat.";
+}
+
+function showCat4() {
+  document.getElementById("highlight").innerHTML =
+    "A cat enjoying the sun.";
+}
+
+function showCat5() {
+  document.getElementById("highlight").innerHTML =
+    "Cool cat.";
+}
