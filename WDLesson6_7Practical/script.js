059927b0e3c836677581daaca3f56a4bafb87267
noob6666666 <oweny19@nycstudents.net>
2026-05-04 17:26:50 +0000

    newv4

--- WDLesson6_7Practical/script.js
+++ WDLesson6_7Practical/script.js
@@ -1,7 +1,7 @@
 let data, info; // global variables
 
 async function init(){  
-  let link = "mvc.json"; //let link = "https://data.cityofnewyork.us/resource/erm2-nwe9.json?$limit=1000";
+  let link = "mvc.json"; //let link = "https://data.cityofnewyork.us/resource/h9gi-nx95.json?$limit=1000";
   info = await fetch(link);
   data = await info.json();
   //console.log(data);
@@ -16,13 +16,12 @@ async function init(){
     build += `<div class="fitted card">
                  <h3>${complaint.on_street_name}</h3>
                  <hr>
+                 <p>${complaint.crash_date}</p>
                  <p>${complaint.crash_time}</p>
+                 <p>${complaint.borough}</p>
+                 <p>${complaint.zip_code}</p>
                  <p>${complaint.number_of_persons_injured}</p>
                  <p>${complaint.vehicle_type_code1}</p>
-                 <hr>
-                 <p>${complaint.vehicle_type_code2}</p>
-                 <hr>
-                 <p>${complaint.vehicle_type_code3}</p>
               </div>`;
     ct++;
   }
@@ -30,66 +29,81 @@ async function init(){
   output.innerHTML = build;
 
   //Filling Dropdown Menus
-  let complaint_types = fillDropDown("complaint_type");
-  document.getElementById("complaint_types").innerHTML = complaint_types;
+  let boroughs = fillDropDown("borough");
+  document.getElementById("boroughs").innerHTML = boroughs;
+  document.getElementById("boroughs2").innerHTML = boroughs;
 
-  let descriptors = fillDropDown("descriptor");
-  document.getElementById("descriptors").innerHTML = descriptors;
-
-  let agencies = fillDropDown("agency_name");
-  document.getElementById("agencies").innerHTML = agencies;  
+  let vehicle_types = fillDropDown("vehicle_type_code1");
+  document.getElementById("vehicle_types").innerHTML = vehicle_types;  
 }
 
-function filterByZipAndAgency(){
-  let zip = document.getElementById("zip").value;
-  let agency = document.getElementById("agencies").value;
+function filterByBorough(){
+  let borough = document.getElementById("boroughs").value;
   let build = "";
   let ct = 0;
 
   for(let i = 0; i < data.length; i+=1){
     let complaint = data[i];
-    if(complaint.incident_zip == zip && complaint.agency_name == agency){
+    if(complaint.borough == borough){
       build += `<div class="fitted card">
-                    <h3>${complaint.complaint_type}</h3>
+                    <h3>${complaint.on_street_name}</h3>
                     <hr>
+                    <p>${complaint.crash_date}</p>
+                    <p>${complaint.crash_time}</p>
                     <p>${complaint.borough}</p>
-                    <p>${complaint.incident_zip}</p>
-                    <p>${complaint.descriptor}</p>
-                    <hr>
-                    <p>${complaint.created_date}</p>
-                    <hr>
-                    <p>${complaint.agency}</p>
+                    <p>${complaint.zip_code}</p>
+                    <p>${complaint.number_of_persons_injured}</p>
+                    <p>${complaint.vehicle_type_code1}</p>
                 </div>`;
       ct += 1;
     }
   }
   result.innerHTML = `${ct} Results found`;
   output.innerHTML = build;
-}
+} 
 
-function filterByComplaintOrDescriptor(){
-  let complaint_type = document.getElementById("complaint_types").value;
-  let descriptor = document.getElementById("descriptors").value;
+function filterByBoroughAndVehicle(){
+  let borough = document.getElementById("boroughs2").value;
+  let vehicle_type = document.getElementById("vehicle_types").value;
   let build = "";
   let ct = 0;
 
   for(let i = 0; i < data.length; i+=1){
     let complaint = data[i];
-    if(complaint.complaint_type == complaint_type || complaint.descriptor == descriptor){
+    if(complaint.borough == borough && complaint.vehicle_type_code1 == vehicle_type){
       build += `<div class="fitted card">
-                    <h3>${complaint.complaint_type}</h3>
+                    <h3>${complaint.on_street_name}</h3>
                     <hr>
+                    <p>${complaint.crash_date}</p>
+                    <p>${complaint.crash_time}</p>
                     <p>${complaint.borough}</p>
-                    <p>${complaint.incident_zip}</p>
-                    <p>${complaint.descriptor}</p>
-                    <hr>
-                    <p>${complaint.created_date}</p>
-                    <hr>
-                    <p>${complaint.agency}</p>
+                    <p>${complaint.zip_code}</p>
+                    <p>${complaint.number_of_persons_injured}</p>
+                    <p>${complaint.vehicle_type_code1}</p>
                 </div>`;
       ct += 1;
     }
   }
   result.innerHTML = `${ct} Results found`;
   output.innerHTML = build;  
+}
+//function to fill drop down
+function fillDropDown(field){
+  let list = [];
+  let build = `<option>Select Option</option>`;
+
+  for(let i = 0; i < data.length; i++){
+    let data_field = data[i];
+    if(!list.includes(data_field[field])){
+      list.push(data_field[field]);
+    }
+  }
+
+  list.sort();
+
+  for(let fieldValue of list){
+    build += `<option>${fieldValue}</option>`;
+  }
+
+  return build;
 }
